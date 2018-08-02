ce739921faef0bdba85d0596f99dc5c294158f61
Liz Redford <lizzie.redford@gmail.com>
2018-08-02 14:28:12 -0500

    Add R script used to further clean & take a sample of cleaned sexuality data.

---
+++ sexuality_clean_sample.R
@@ -0,0 +1,14 @@
+# This code was used to take certain variables from, and a random sample of, data from the Gender-Science Implicit Association Test. This data had already been cleaned by Hila Zitelny as part of the same Documenting Bias project.
+
+library(here)
+library(dplyr)
+
+sexualityiatdat <- read.csv("sexuality_cleaned.csv", header = TRUE) %>% select(Implicit, Explicit, racenum, sexnum, politics, datecode, age, year, education, numiats, religiosity) # import data, selecting only needed variables
+#  N = 993 953
+
+sexualityiatdat <- sexualityiatdat[sample(1:nrow(sexualityiatdat), 8000, replace = FALSE),]
+# Take random sample of 8000 rows; about .8%
+
+### Write data to csvs to export ##### ================
+library(readr)
+write_csv(sexualityiatdat, "sexualityiatdat.csv") # Export data to use in Shiny
